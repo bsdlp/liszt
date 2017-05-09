60c9825d0b58fd1ec9bcd6d3b7af6f4ac7b2114e
Jon Chen <bsd@voltaire.sh>
2017-05-09 10:56:38 -0700

    make tests pass

--- pkg/registry/sqlite.go
+++ pkg/registry/sqlite.go
@@ -14,16 +14,26 @@ type registrar struct {
 }
 
 // GetUnitResidents implements registrar
-func (req *registrar) GetUnitResidents(ctx context.Context, unitID string) (residents []*Resident, err error)
+func (req *registrar) GetUnitResidents(ctx context.Context, unitID string) (residents []*Resident, err error) {
+	return
+}
 
 // GetUnitByName implmements registrar
-func (req *registrar) GetUnitByName(ctx context.Context, name string) (unit *Unit, err error)
+func (req *registrar) GetUnitByName(ctx context.Context, name string) (unit *Unit, err error) {
+	return
+}
 
 // RegisterResident implements registrar
-func (req *registrar) RegisterResident(ctx context.Context, resident *Resident, unitID string) (err error)
+func (req *registrar) RegisterResident(ctx context.Context, resident *Resident, unitID string) (err error) {
+	return
+}
 
 // MoveResident implements registrar
-func (req *registrar) MoveResident(ctx context.Context, residentID, newUnitID string) (err error)
+func (req *registrar) MoveResident(ctx context.Context, residentID, newUnitID string) (err error) {
+	return
+}
 
 // DeregisterResident implements registrar
-func (req *registrar) DeregisterResident(ctx context.Context, residentID string) (err error)
+func (req *registrar) DeregisterResident(ctx context.Context, residentID string) (err error) {
+	return
+}
